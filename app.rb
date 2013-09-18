d4e5ef3aafbd16dac9a3f3be507e67a3cc6aaf95
Cullen Murphy <cullen@cullenshane.com>
2013-09-18 16:23:00 -0500

    First working app, doesn't do anything, but all the bits are ready to go.

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'coffee-script'
+require 'net/http'
+#require File.join(File.dirname(__FILE__), '.', 'apruve.rb')
+
+
+get '/' do
+  erb :index
+end
